90938ae24837b7a97c31db04558f4562b595aead
Michael Israel Sousa Santos <43841525+loser-lost@users.noreply.github.com>
2026-09-19 21:40:01 -0300

    Correction

---
+++ src/projects/projects.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ProjectsController } from './projects.controller.js';
+import { ProjectsService } from './projects.service.js';
+
+describe('ProjectsController', () => {
+  let controller: ProjectsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ProjectsController],
+      providers: [ProjectsService],
+    }).compile();
+
+    controller = module.get<ProjectsController>(ProjectsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
--- test.js
+++
@@ -1,3 +0,0 @@
-function TestTs() {
-    return {}
-};
